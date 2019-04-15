9a6c015cefb7a2047a1a206770884890a6fa65c0
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2019-04-15 20:41:06 +0300

    cleaning for view

--- models/cardable.js
+++
@@ -1,9 +0,0 @@
-//cardable interface other models will need to define these fields so that
-//they can be viewed as cards
-
-module.exports = {
-    name: String,
-    thumbnailUrl: String,
-    shortDescription: String,
-    
-}
--- routers/members.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, send) => res.render("members"));
-
-module.exports = router;
