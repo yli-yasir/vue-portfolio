d158f64f269e50ce12862add127701f7e31d2b8f
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2019-06-05 16:24:19 +0300

    env vars

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const methodOverride= require('method-override');
@@ -9,7 +10,7 @@ const passport = require('passport');
 
 //---MONGO---
 mongoose.connect(
-  "mongodb+srv://Yasir:hello1234@cluster0-phxet.mongodb.net/soulera2?retryWrites=true",
+  process.env.MONGO_CONNECTION_STRING,
   { useNewUrlParser: true }
 );
 
@@ -17,22 +18,21 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error!!!"));
 db.once("open", () => {
-  console.log("Connected to mongo...");
-  //require('./utils/seeder.js')
+  console.log("Connected to mongo!");
+//require('./utils/seeder.js')
 });
 //---/MONGO---
 
 app.use(cookieParser());
 app.use(passport.initialize());
-
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static("public"));
 require('./utils/auth.js')(passport);
 
 
 
- app.use(methodOverride('_method'));
 
- app.use(bodyParser.urlencoded({ extended: true }));
- app.use("/public", express.static("public"));
 
 //Use routers 
 app.use("/api", apiRouter);
@@ -41,5 +41,5 @@ app.get("/*", (req, res, next) => {
   res.sendFile( __dirname + "/index.html")
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT;
  app.listen(port, () => console.log(`the app has started on port: ${port}`));
