8032f53e71da0e28b3571bed01ff8c2b0bc537f4
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2019-06-09 19:52:19 +0300

    secure cookie

--- routers/users.js
+++ routers/users.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const UserModel = require('../models/user');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
+const UserModel = require("../models/user");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
-async function bodyToDocument(body){
-    const _id = body.username
-    try{
-    const password = await bcrypt.hash(body.password,10)
+async function bodyToDocument(body) {
+  const _id = body.username;
+  try {
+    const password = await bcrypt.hash(body.password, 10);
     return {
-        _id,
-        password
+      _id,
+      password
     };
-}
-    catch(error){
-      next(error);
-    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 // //todo remove this
@@ -29,23 +28,27 @@ async function bodyToDocument(body){
 //     }
 //   });
 
-
-router.post('/',async (req,res,next)=> {
-    try {
-        const document = await bodyToDocument(req.body);
-        await new UserModel(document).save();
-        res.status(201).send('resource created')
-    }
-    catch(error){
-      next(error);
-    }
+router.post("/register", async (req, res, next) => {
+  try {
+    const document = await bodyToDocument(req.body);
+    await new UserModel(document).save();
+    res.status(201).send("resource created");
+  } catch (error) {
+    next(error);
+  }
 });
 
-
-router.post("/login", passport.authenticate("local",{session:false}), function(req, res) {
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  function(req, res) {
     const token = jwt.sign(req.user.toJSON(), process.env.SECRET);
-    res.cookie('token',token);
-    res.send('token granted')
-  });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.SECURE_COOKIE === 'true' ? true : false
+    });
+    res.send("token granted");
+  }
+);
 
 module.exports = router;
