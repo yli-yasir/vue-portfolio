b4fc927511328dbb81643360867f614a7995fcfa
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2019-06-09 17:41:48 +0300

    helmet + rate-limit

--- server.js
+++ server.js
@@ -1,41 +1,51 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const methodOverride= require('method-override');
-const cookieParser = require('cookie-parser');
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const passport = require('passport');
+const passport = require("passport");
 
 //---MONGO---
-mongoose.connect(
-  process.env.MONGO_CONNECTION_STRING,
-  { useNewUrlParser: true }
-);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true
+});
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Connection error!!!"));
 db.once("open", () => {
   console.log("Connected to mongo!");
-//require('./utils/seeder.js')
+  //require('./utils/seeder.js')
 });
 //---/MONGO---
 
+app.use(helmet());
+app.use(
+  rateLimit("/api/",{
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(cookieParser());
 app.use(passport.initialize());
-require('./config/passport.js')(passport);
-app.use(methodOverride('_method'));
+require("./config/passport.js")(passport);
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
 
-//Use routers 
+//Use routers
 app.use("/api/members", require("./routers/members.js"));
 app.use("/api/projects", require("./routers/projects.js"));
 app.use("/api/news", require("./routers/news.js"));
-app.use("/api/users",require("./routers/users.js"))
+app.use("/api/users", require("./routers/users.js"));
 //todo change this when you are done with the front end
 app.get("/*", (req, res, next) => {
-  res.sendFile( __dirname + "/index.html")
+  res.sendFile(__dirname + "/index.html");
 });
 
- app.listen( process.env.PORT, () => console.log('the app has started on port:' +  process.env.PORT));
+app.listen(process.env.PORT, () =>
+  console.log("the app has started on port:" + process.env.PORT)
+);
